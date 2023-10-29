3975d988bbc8cfd82a7fde70c03b2c17b7280280
BilalUddin <c3071276@hallam.shu.ac.uk>
2023-10-29 11:18:26 +0000

    Player class created

---
+++ Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace othello
+{
+    internal class Player
+    {
+        protected string __name;
+        protected int __tokenCount;
+        protected int __id;
+
+        public string Name
+        {
+            get => __name;
+        }
+
+        public int TokenCount
+        {
+            get => __tokenCount;
+        }
+
+        public int ID
+        {
+            get => __id;
+        }
+
+        public Player(string name, int tokenCount, int id)
+        {
+            this.__name = name;
+            this.__tokenCount = tokenCount;
+            this.__id = id;
+        }
+    }
+}
